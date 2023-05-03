8148e79bc90a7c9122647e4607e8d749a735bb27
mauriciolasgon <108753106+mauriciolasgon@users.noreply.github.com>
2023-05-02 23:03:49 -0300

    Add files via upload

--- conta.py
+++ conta.py
@@ -4,16 +4,20 @@ nos=[]
 ligas=[]
 forcas=[]
 apoios=[]
+fr=[]
 def main(): 
     escolha=menu()
     adiciona(escolha,nos,ligas)
-    print(nos)
+
 
 
 def menu():
     print("Para adicionar nós digite 1","\nPara ligar os nós digite 2","\nPara adicionar forças digite 3",
-          "\nPara selecionar os apoios digite 4 ")
+          "\nPara selecionar os apoios digite 4 ","\n Para encontrar as forças digite 5 ")
     escolha=input()
+    if escolha=="5":
+        print("ola")
+        calculo()
     return escolha
 
 def adiciona(escolha,nos,ligas):
@@ -25,8 +29,8 @@ def adiciona(escolha,nos,ligas):
             x,y=input(f"Digite as coordenadas do nó {i} ")
             print('coordenada x:',x)
             print('coordenada y:',y)
-            nos.append(x)
-            nos.append(y)
+            nos.append([x,y])
+            
             print(nos)
             for v in enumerate(nos) :
                 print(v)
@@ -41,11 +45,11 @@ def adiciona(escolha,nos,ligas):
             h1,h2=input("Digite os nós que deseja conectar, na ordem")
             for i,v in enumerate(nos):
                 if i==int(h1)-1:
-                    n1.append(nos[i])
-                    n1.append(nos[i+1])
+                    n1.append(nos[0])
+                    n1.append(nos[1])
                 if i==int(h2)-1:
-                    n2.append(nos[i])
-                    n2.append(nos[i+1])
+                    n2.append(nos[0])
+                    n2.append(nos[1])
                 
             print(n1,n2)
             ligas.append([n1,n2])
@@ -54,19 +58,40 @@ def adiciona(escolha,nos,ligas):
     if escolha=="3":
         while continuar=="1":
             n,f,theta=input("Escolha o nó que deseja aplicar a força,seu módulo e seu ângulo de inclinação(em graus) com o eixo x:")
+            s=input("Qual o sentido da força? (c,b)")
             theta=60*(np.pi)/180
-            fx=float(f) *(np.cos(theta))
-            fy=float(f)* (np.sin(theta))
+            if s=="c":
+                fx=float(f) *(np.cos(theta))
+                fy=float(f)* (np.sin(theta))
+            else:
+                fx=float(f) *(-np.cos(theta))
+                fy=float(f)* (-np.sin(theta))
+
             forcas.append([n,fx,fy])
             print(forcas)
             print(forcas[0])
             continuar=input("Para adiconar outra força digite 1")
+        main()
 
     if escolha=="4":
         while continuar=="1":
             a=input("Esolha o tipo de apoio que deseja adicionar: \nApoio móvel\nApoio fixo\nSuperapoio\n")
             apoios.append(a)
             print(apoios)
-            menu()
-
+            main()
+def calculo():
+    fx=0
+    fy=0
+    for i,n in enumerate(nos):    
+        for f in forcas:
+            if int(f[0])==i+1:
+                fx=fx+f[1]
+                fy=fy+f[2]
+            
+        fr.append([i+1,fx,fy])
+    print(fr)
+    main()
+            
+                
+            
 main()
