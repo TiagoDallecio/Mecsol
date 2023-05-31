bf5132ae10e3aa07e68089061b321b97f7ebfab4
TiagoDallecio <tidallecio@hotmail.com>
2023-05-31 00:20:29 -0300

    Co-authored-by: mauriciolasgon <mauriciolasgon@users.noreply.github.com>

--- conta.py
+++ conta.py
@@ -1,10 +1,13 @@
 import numpy as np
+import sys
+import time
 
 nos=[]
 ligas=[]
 forcas=[]
 apoios=[]
 fr=[]    
+nos_visitados=[]
 def main():
     escolha=menu()
     if escolha=="5":
@@ -12,42 +15,69 @@ def main():
     adiciona(escolha,nos,ligas)
 
 
-def menu():
+def menu(): # menu de seleção de 'operações'
     print("Para adicionar nós digite 1","\nPara ligar os nós existentes digite 2","\nPara adicionar forças digite 3",
           "\nPara opções de apoios digite 4","\nPara consultar as forças digite 5","\nPara sair digite 6")
     escolha=input("=> ")
     return escolha
 
-def adiciona(escolha,nos,ligas):
+def adiciona(escolha,nos,ligas): # função chamada pelo menu que executa a opção selecionada
     continuar='1'
-    if escolha=="1":
+    if escolha=="1": # implementação de nós por meio de um sistema de coordenadas x,y
         i=0
         while continuar=='1':
             i+=1
-            x,y=input(f"Digite as coordenadas do nó {i} => ")
-            print('coordenada x:',x)
+            print("Para voltar para o menu digite 'sair' \n")
+            x=input(f"Digite a coordenadas 'x' do nó {i} => ")
+            if x =='sair':
+                break
+            y=input(f"Digite a coordenadas 'y' do nó {i} => ")
+            if y =='sair':
+                break
+            print('\ncoordenada x:',x)
             print('coordenada y:',y)
-            nos.append([x,y])
+            n=[x,y]
+            if n in nos:
+                print("\nEsse nó ja existe na treliça!!\n")
+                i-=1
+                time.sleep(2)
+                continuar=input("Para adiconar outro nó digite 1 => ")
+                
+            else:
+                nos.append(n)
            
-            print(nos)
-            for v in enumerate(nos) :
-                print(v)
-            continuar=input("Para adiconar outro nó digite 1")
+                print(nos)
+                for v in enumerate(nos) :
+                    print(v)
+                continuar=input("Para adicionar outro nó digite 1 => ")
         main()
     
-    if escolha == "2" and len(nos) > 1:
+    if escolha == "2" and len(nos) > 1: # validação do número de elementos presentes na lista de nós
         n1 = []
         n2 = []
         barra=[]
-        while continuar == "1":
+        while continuar == "1": # caso passe pela validação, cria um loop para o menu de criação de barras entre os nós
             print(nos)  
-            h1, h2 = input("Digite os nós que deseja conectar, na ordem => ")
+            print("\nDigite 'sair' em qualquer um dos campos para voltar ao menu\n")
+            print("Digite os nós que deseja conectar, na ordem")
+
+            h1 = input("Nó inicial: ")
+            if h1 == 'sair':
+                main()
+                break
+
+            h2 = input("Nó final: ")
+            if h2 == 'sair':
+                main()
+                break
+
             h1, h2 = int(h1), int(h2) 
             for i, v in enumerate(nos):
                 if i == h1 - 1:
                     n1.append(nos[i])
                 if i == h2 - 1:
                     n2.append(nos[i])
+                    
             if float(n2[0][0])-float(n1[0][0])!=0:
                 tg=(float(n2[0][1])-float(n1[0][1]))/(float(n2[0][0])-float(n1[0][0]))
                 ang=np.arctan(tg)
@@ -56,51 +86,99 @@ def adiciona(escolha,nos,ligas):
             barra=[h1-1,h2-1,ang]
             print((ang*180)/np.pi)
             print(n1, n2)
-            viga = np.sqrt((float(n2[0][0])-float(n1[0][0]))**2 + (float(n2[0][1])-float(n1[0][1]))**2)
-            ligas.append(barra)     #matriz das barras
+            viga = np.sqrt((float(n2[0][0])-float(n1[0][0]))**2 + (float(n2[0][1])-float(n1[0][1]))**2) # conta que cálcula o tamanho da barra gerada ao ligar dois nós 
+            ligas.append(barra)     # matriz das barras
             print(ligas)
-
-            print(f"Tamanho da viga gerada: {viga}")
-            main()
-    elif escolha == "2" and len(nos) < 1:
-        print("É preciso ter, no mínimo, 2 nós para usar essa opção")
-    if escolha=="3":
+            print(f"\nUma barra foi gerada entre os nós {h1} e {h2}!")
+            print(f"\nTamanho da barra gerada: {viga}\n")
+            time.sleep(2)
+            continuar = input("Para criar mais uma barra digite 1 => ")
+        main()
+    elif escolha == "2" and len(nos) < 1: # caso não passe pela validação, retorna um aviso e redireciona para o menu principal
+        print("\nÉ preciso ter, no mínimo, 2 nós para usar essa opção\n")
+        time.sleep(2)
+        main()
+    if escolha=="3": # menu de inserção de forças
         while continuar=="1":
-            n,f,theta=input("Escolha o nó em que deseja aplicar a força, módulo da mesma e seu ângulo de inclinação(em graus) com o eixo x: ") #ver com o mauritz
-            s,d=input("Qual a direção e o sentido da força? (v,h)(c,b,d,e)")
-            theta=float(theta)*(np.pi)/180
-            if d=="c" and s=="d":
-                fx=float(f) *(np.cos(theta))
-                fy=float(f) *(np.sin(theta))
-            elif d=="c" and s=="e":
-                fx=float(f) *(-np.cos(theta))
-                fy=float(f)* (np.sin(theta))
-            elif d=="b" and s=="d":
-                fx=float(f) *(np.cos(theta))
-                fy=float(f)* (-np.sin(theta))
-            else:
-                fx=float(f) *(-np.cos(theta))
-                fy=float(f)* (-np.sin(theta))
+            if len(nos) > 0: # validação da quantidade de nós presentes na lista
+                print("\nDigite 'sair' para voltar ao menu\n")
+                n=input("Escolha o nó em que deseja aplicar a força: ") #ver com o mauritz
+                if n == 'sair':
+                    break
+                f=input("Módulo da força: ")
+                theta=input("Ângulo de inclinação(em graus) da força com o eixo x: ")
+                s,d=input("Qual a direção e o sentido da força? (d,e)(c,b): ")
+                theta=float(theta)*(np.pi)/180 # conversão do angulo (graus para radianos)
+                if d=="c" and s=="d": # diferentes validações para determinar o sinal das forças
+                    fx=float(f) *(np.cos(theta))
+                    fy=float(f) *(np.sin(theta))
+                elif d=="c" and s=="e":
+                    fx=float(f) *(-np.cos(theta))
+                    fy=float(f)* (np.sin(theta))
+                elif d=="b" and s=="d":
+                    fx=float(f) *(np.cos(theta))
+                    fy=float(f)* (-np.sin(theta))
+                else:
+                    fx=float(f) *(-np.cos(theta))
+                    fy=float(f)* (-np.sin(theta))
 
 
-            forcas.append([n,fx,fy])
-            print(forcas)
-            print(forcas[0])
-            continuar=input("Para adicionar outra força digite 1 ")
+                forcas.append([n,fx,fy]) # adiciona, respectivamente, o módulo, componente em x e em y da força na lista 'forcas'
+                print(forcas)
+                print(forcas[0])
+                continuar=input("Para adicionar outra força digite 1 ")
+            else: # caso não passe pela validação, retorna mensagem e redireciona para o menu principal
+                print("\nÉ preciso ter pelo menos um nó para declarar uma força.\n")
+                time.sleep(2)
+                main()
+                break
         main()
 
-    if escolha=="4":
+    if escolha == "4": # menu de inserção de apoios
         while continuar=="1":
-            a=int(input("Esolha o tipo de apoio que deseja adicionar : \n1-Apoio móvel\n2-Apoio fixo\n3-Engastamento\n"))
-            n=input("Em que nó ele se encontra?")
-            apoios.append([n,a])
-            print(apoios)
-            main()
-
-    if escolha =="6":
+            if len(nos) > 0: # validação do número de nós na lista, necessário pelo menos um para a inserção do apoio
+                print("\nDigite 'sair' para voltar ao menu\n")
+                a=input("Esolha o tipo de apoio que deseja adicionar : \n1-Apoio móvel\n2-Apoio fixo\n3-Engastamento\n ")
+                if a == 'sair':
+                    main()
+                    break
+                else:
+                    a = int(a) 
+                n=input("Em que nó ele se encontra? ")
+                erro=0
+                num_nos=len(nos)
+                if int(n)>num_nos:
+                    print("Esse nó não existe!!")
+                    continuar=input("Para adicionar outro apoio digite 1 => ")
+                else:
+                    for i,ap in enumerate(apoios):
+                        if ap[0]==n:
+                            r=input("Já existe um apoio nesse nó! Deseja sobrescrever?(sim/nao) \n")
+                            erro=1
+                            if r=='nao':
+                                continuar=input("Para adicionar outro apoio digite 1 => ")
+                            elif r=='sim':
+                                apoios[i]=[n,a] # caso passe pela validação, insere o apoio escolhido e o nó selecionado na lista de apoios 
+                                print(apoios)
+                                continuar=input("Para adicionar outro apoio digite 1 => ")
+                    if erro==0:
+                        apoios.append([n,a]) # caso passe pela validação, insere o apoio escolhido e o nó selecionado na lista de apoios
+                        print(apoios)
+                        continuar=input("Para adicionar outro apoio digite 1 => ")
+                
+                
+            
+            else: # caso não passe pela validação, redireciona para o menu principal com uma mensagem
+                print("\nÉ preciso ter pelo menos um nó para posicionar um apoio.\n")
+                time.sleep(2)
+                main()
+                break
+        main()
+    
+    if escolha =="6": # créditos
         print("\n\nFeito por Tiago Oliveira Dallecio, Mauricio Lasca Gonçales e Murilo Alves Croce\nEngenharia De Computação\nPUC-Campinas 2023\n\n")
-        return 0
-def calculo():
+        sys.exit()
+def calculo(): # função para o cálculo de forças 
     fx=0
     fy=0
     coord=[]
@@ -120,43 +198,51 @@ def calculo():
         fx=0
         fy=0
     num_nos=len(nos)
-    matriz_focas=np.zeros((2*num_nos,2*num_nos))
+    num_barras=len(ligas)
+    matriz_forcas=np.zeros((2*num_nos,2*num_nos))
     vetor_carga_nos=np.zeros(2*num_nos)
     
     for no,modulo in enumerate(nos):
         for f in fr:
             if int(f[0])==no and f[1] is not None :
                 vetor_carga_nos[2*no]=f[1]
-                matriz_focas[2*no,2*no]=1
+                matriz_forcas[2*no,2*no]=1
 
             if int(f[0])==no and f[2] is not None:
-                vetor_carga_nos[2*no+1]=f[1]
-                matriz_focas[2*no+1,2*no+1]=1
+                vetor_carga_nos[2*no+1]=f[2]
+                matriz_forcas[2*no+1,2*no+1]=1
     
-    for i,l in enumerate(ligas):
-        no_i=l[0]
-        no_f=l[1]
-        cos=np.cos(l[2])
-        sen=np.sin(l[2])
+        for i,l in enumerate(ligas):
+            no_i=l[0]
+            no_f=l[1]
+            cos=np.cos(l[2])
+            sen=np.sin(l[2])
+
+            #considerando que o eixo x segue a orientação da barra
+            #força x
+            matriz_forcas[2*no_i, 2*i] = cos
+            matriz_forcas[2*no_i, 2*i + 1] = sen
+            #força y
+            matriz_forcas[2*no_i+1, 2*i] = -sen
+            matriz_forcas[2*no_i+1, 2*i + 1] = cos
+            #força x nof
+            matriz_forcas[2*no_f, 2*i] = -cos
+            matriz_forcas[2*no_f, 2*i + 1] = -sen
+            #força y
+            matriz_forcas[2*no_i+1, 2*i] = sen
+            matriz_forcas[2*no_i+1, 2*i + 1] = -cos
+
+            #ainda nao da certo
 
 
-         #força x
-        matriz_focas[2*no_i, 2*i] = cos
-        matriz_focas[2*no_i, 2*i + 1] = sen
-        #força y
-        matriz_focas[2*no_i+1, 2*i] = -sen
-        matriz_focas[2*no_i+1, 2*i + 1] = cos
-        #força x nof
-        matriz_focas[2*no_f, 2*i] = -cos
-        matriz_focas[2*no_f, 2*i + 1] = -sen
-
-
-
-        solucao=np.linalg.solve(matriz_focas, vetor_carga_nos)
-
+            solucao=np.linalg.solve(matriz_forcas, vetor_carga_nos)
+        
+        forcas_barras = solucao[:2*num_nos]
         print(vetor_carga_nos)
-        print(matriz_focas)
+        print(matriz_forcas)
         print(solucao)
+        print(forcas_barras)
+
     
     """ 
     for ap in apoios:
@@ -198,5 +284,5 @@ def calculo():
     print(fr)
     main()
             
-      
+            
 main()
