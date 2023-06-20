30095fefa2ccd7ec822bdf78634eca4f2e3ce7c1
TiagoDallecio <tidallecio@hotmail.com>
2023-06-19 21:16:57 -0300

    Update conta.py
    Detecção de intersecção e implementação dos calculos

--- conta.py
+++ conta.py
@@ -1,6 +1,25 @@
 import numpy as np
 import sys
 import time
+import matplotlib.pyplot as plt
+
+def intersect(line1, line2):
+    x1, y1 = line1[0]
+    x2, y2 = line1[1]
+    x3, y3 = line2[0]
+    x4, y4 = line2[1]
+
+    den = (x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4)
+    if den == 0:
+        return False
+
+    t = ((x1 - x3)*(y3 - y4) - (y1 - y3)*(x3 - x4)) / den
+    u = -((x1 - x2)*(y1 - y3) - (y1 - y2)*(x1 - x3)) / den
+
+    if 0 <= t <= 1 and 0 <= u <= 1:
+        return True
+    else:
+        return False
 
 nos=[]
 ligas=[]
@@ -11,7 +30,7 @@ nos_visitados=[]
 def main():
     escolha=menu()
     if escolha=="5":
-        calculo()
+        calcular_forcas(nos, forcas, ligas)
     adiciona(escolha,nos,ligas)
 
 
@@ -87,12 +106,34 @@ def adiciona(escolha,nos,ligas): # função chamada pelo menu que executa a opç
             print((ang*180)/np.pi)
             print(n1, n2)
             viga = np.sqrt((float(n2[0][0])-float(n1[0][0]))**2 + (float(n2[0][1])-float(n1[0][1]))**2) # conta que cálcula o tamanho da barra gerada ao ligar dois nós 
-            ligas.append(barra)     # matriz das barras
-            print(ligas)
-            print(f"\nUma barra foi gerada entre os nós {h1} e {h2}!")
-            print(f"\nTamanho da barra gerada: {viga}\n")
-            time.sleep(2)
-            continuar = input("Para criar mais uma barra digite 1 => ")
+            # Verificação de interseção de barras
+            intersection = False
+
+            for i in range(len(ligas)):
+                line1 = [(float(nos[ligas[i][0]][0]), float(nos[ligas[i][0]][1])),
+                         (float(nos[ligas[i][1]][0]), float(nos[ligas[i][1]][1]))]
+                line2 = [(float(n1[0][0]), float(n1[1][0])),
+                         (float(n2[0][0]), float(n2[1][0]))]
+                
+                if (line1[0] == line2[0] or line1[0] == line2[1] or
+                    line1[1] == line2[0] or line1[1] == line2[1]):
+                    continue
+                
+                if intersect(line1, line2):
+                    intersection = True
+                    break
+
+            if intersection:
+                print("Verifique o input, intersecção de barras detectada")
+                time.sleep(2)
+                break
+            else:
+                ligas.append(barra)     # matriz das barras
+                print(ligas)
+                print(f"\nUma barra foi gerada entre os nós {h1} e {h2}!")
+                print(f"\nTamanho da barra gerada: {viga}\n")
+                time.sleep(2)
+                continuar = input("Para criar mais uma barra digite 1 => ")
         main()
     elif escolha == "2" and len(nos) < 1: # caso não passe pela validação, retorna um aviso e redireciona para o menu principal
         print("\nÉ preciso ter, no mínimo, 2 nós para usar essa opção\n")
@@ -178,111 +219,66 @@ def adiciona(escolha,nos,ligas): # função chamada pelo menu que executa a opç
     if escolha =="6": # créditos
         print("\n\nFeito por Tiago Oliveira Dallecio, Mauricio Lasca Gonçales e Murilo Alves Croce\nEngenharia De Computação\nPUC-Campinas 2023\n\n")
         sys.exit()
-def calculo(): # função para o cálculo de forças 
-    fx=0
-    fy=0
-    coord=[]
-    dist=[]
-    vet=[]
-    nó=0
-    momento=0
-    countx=0
-    county=0
-    for i,n in enumerate(nos):    
-        for f in forcas:
-            if int(f[0])==i+1 and isinstance(f[1], float):
-                fx=fx+f[1]
-                fy=fy+f[2]  #resultante das forças
-        
-        fr.append([i,fx,fy])
-        fx=0
-        fy=0
-    num_nos=len(nos)
-    num_barras=len(ligas)
-    matriz_forcas=np.zeros((2*num_nos,2*num_nos))
-    vetor_carga_nos=np.zeros(2*num_nos)
-    
-    for no,modulo in enumerate(nos):
-        for f in fr:
-            if int(f[0])==no and f[1] is not None :
-                vetor_carga_nos[2*no]=f[1]
-                matriz_forcas[2*no,2*no]=1
-
-            if int(f[0])==no and f[2] is not None:
-                vetor_carga_nos[2*no+1]=f[2]
-                matriz_forcas[2*no+1,2*no+1]=1
-    
-        for i,l in enumerate(ligas):
-            no_i=l[0]
-            no_f=l[1]
-            cos=np.cos(l[2])
-            sen=np.sin(l[2])
-
-            #considerando que o eixo x segue a orientação da barra
-            #força x
-            matriz_forcas[2*no_i, 2*i] = cos
-            matriz_forcas[2*no_i, 2*i + 1] = sen
-            #força y
-            matriz_forcas[2*no_i+1, 2*i] = -sen
-            matriz_forcas[2*no_i+1, 2*i + 1] = cos
-            #força x nof
-            matriz_forcas[2*no_f, 2*i] = -cos
-            matriz_forcas[2*no_f, 2*i + 1] = -sen
-            #força y
-            matriz_forcas[2*no_i+1, 2*i] = sen
-            matriz_forcas[2*no_i+1, 2*i + 1] = -cos
-
-            #ainda nao da certo
 
 
-            solucao=np.linalg.solve(matriz_forcas, vetor_carga_nos)
-        
-        forcas_barras = solucao[:2*num_nos]
-        print(vetor_carga_nos)
-        print(matriz_forcas)
-        print(solucao)
-        print(forcas_barras)
+def calcular_forcas(nos, forcas, ligas):
+    fx = np.zeros(len(nos))
+    fy = np.zeros(len(nos))
+    for f in forcas:
+        no, x, y = f
+        fx[int(no)-1] += float(x)
+        fy[int(no)-1] += float(y)
 
-    
-    """ 
-    for ap in apoios:
-        vet.append(ap[1])
-    print(fr)
-    ap=max(vet, key=int)    #maior numero de variaveis entre os apoios
-    for a in apoios:
-        if a[1]==ap:                #momento é um produto vetorial
-            nó=a[0]
-    """
-    for i,n in enumerate(nos):
-        if i+1==int(nó):             #coloca as coordenadas do nó na variável nó
-            coordnó=n 
-        
-        for f in forcas:
-            if int(f[0])==i+1:
-                coord.append(n)
-               
-    print(coord)
-    print(coordnó)
-    
-                        #resolver questão do módulo
-      
-    for c in coord:         #achando as distâncias das forças com o apoio
-        x=int(c[0])-int(coordnó[0])
-        y=int(c[1])-int(coordnó[1])
-        dist.append([x,y])
+    num_nos = len(nos)
+    num_barras = len(ligas)
+    matriz_forcas = np.zeros((2 * num_nos, 2 * num_barras))
+    vetor_carga_nos = np.zeros(2 * num_nos)
 
-    for i,f in enumerate(forcas):   #componente da força no vetor m                           
-        m=[]                        #produto vetorial do vetor das forças com o vetor das distâncias
-        m.append(f[1])
-        m.append(f[2])
-        momento=momento+np.cross(m,dist[i])
-    
-    print(momento)
+    for i, (no_i, no_f, angulo) in enumerate(ligas):
+        no_i -= 1
+        no_f -= 1
+        cos = np.cos(angulo)
+        sen = np.sin(angulo)
 
-        
-    print(dist)        
-    print(fr)
-    main()
-            
+        # força x
+        matriz_forcas[2 * no_i, 2 * i] = cos
+        matriz_forcas[2 * no_i, 2 * i + 1] = sen
+        matriz_forcas[2 * no_f, 2 * i] = -cos
+        matriz_forcas[2 * no_f, 2 * i + 1] = -sen
+
+        # força y
+        matriz_forcas[2 * no_i + 1, 2 * i] = -sen
+        matriz_forcas[2 * no_i + 1, 2 * i + 1] = cos
+        matriz_forcas[2 * no_f + 1, 2 * i] = sen
+        matriz_forcas[2 * no_f + 1, 2 * i + 1] = -cos
+
+    for no, (x, y) in enumerate(zip(fx, fy)):
+        vetor_carga_nos[2 * no] = x
+        vetor_carga_nos[2 * no + 1] = y
+
+    print(matriz_forcas)
+    print(num_nos)
+    print(num_barras)
+    matriz_forcas = np.where(np.abs(matriz_forcas) < 1e-10, 0, matriz_forcas)
+    print(matriz_forcas)
+
+    determinante = np.linalg.det(matriz_forcas)
+
+    if abs(determinante) < 1e-10:
+        solucao = np.linalg.lstsq(matriz_forcas, vetor_carga_nos, rcond=None)
+        solucao = solucao[0]  # Acesso ao resultado da solução
+    else:
+        solucao = np.linalg.solve(matriz_forcas, vetor_carga_nos)
+
+    for no in range(num_nos):
+        forca_x = solucao[2 * no]  # Acessar o elemento correto da solução
+        forca_y = solucao[2 * no + 1]  # Acessar o elemento correto da solução
+        print(f"No {no+1}: Força X = {forca_x}N, Força Y = {forca_y}N")
+
+'''
+
+
+'''
+
             
 main()
